0ac3c43579242f25df0c01892cddd00f93139d99
pc <a@mail>
2023-10-07 09:57:31 +0500

    first commit

---
+++ bin/okt7.dart
@@ -0,0 +1,5 @@
+import 'package:okt7/okt7.dart' as okt7;
+
+void main(List<String> arguments) {
+  print('Hello world: ${okt7.calculate()}!');
+}
---
+++ test/okt7_test.dart
@@ -0,0 +1,8 @@
+import 'package:okt7/okt7.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
